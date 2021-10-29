2988f253e858e017e80acffeaad3888c248343d6
mateosceia <mateosceia.ms@gmail.com>
2021-10-29 15:24:17 -0300

    login

--- minka/src/main.js
+++ minka/src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import HomePage from "./components/HomePage";
+import Login from "./components/Login";
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
 
 const routes = [
-  {path:'/', name:"HomePage", component:HomePage}
+  {path:'/', name:"HomePage", component:HomePage},
+  {path:'/login', name:"Login", component:Login}
 ]
 const router = new VueRouter({
   routes
