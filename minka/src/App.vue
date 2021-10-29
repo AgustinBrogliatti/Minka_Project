db5363d59c1735cea973d99402bcf9971ea0b275
mateosceia <mateosceia.ms@gmail.com>
2021-10-29 14:50:57 -0300

    router

--- minka/src/App.vue
+++ minka/src/App.vue
@@ -1,37 +1,22 @@
 <template>
   <div id="app">
-    <Header></Header>
-    <div id="content-body">
-      <NavBar></NavBar>
-      <Home></Home>
-      <Banner></Banner>
-    </div>
-    <Footer></Footer>
+  <router-view></router-view>
   </div>
 </template>
 
 <script>
 
-import Header from "@/components/Header";
-import NavBar from "@/components/NavBar";
-import Home from "@/components/Home";
-import Banner from "@/components/Banner";
-import Footer from "@/components/Footer";
+
 
 export default {
   name: 'App',
   components: {
-    Header,
-    NavBar,
-    Home,
-    Banner,
-    Footer,
 
   }
 }
 </script>
 
 <style scoped>
-@import "./assets/CSS/normalize.css"
+@import "./assets/CSS/normalize.css";
 
 </style>
--- minka/src/main.js
+++ minka/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import HomePage from "./components/HomePage";
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
 
+const routes = [
+  {path:'/', name:"HomePage", component:HomePage}
+]
+const router = new VueRouter({
+  routes
+})
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
