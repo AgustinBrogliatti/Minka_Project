c8aa07ea97a8884cd4509f9e0484dfab191aaf6e
MartinoBoca <bocamartino1201@gmail.com>
2021-11-03 18:51:20 -0300

    project info objectList

--- minka/src/components/ProjectInfo.vue
+++ minka/src/components/ProjectInfo.vue
@@ -4,14 +4,18 @@
       <form>
         <legend> <h1> Datos del proyecto </h1> </legend>
         <hr>
-        <label for="fname">Nombre del cliente</label>
-        <input type="text" id="fname" name="firstname">
+
+        <label>Nombre del proyecto</label>
+        <input type="text" id="project_name" v-model="nombre_proyecto">
+
+        <label>Nombre del cliente</label>
+        <input type="text" id="fname" name="firstname" v-model="nombre">
 
         <label for="lname">Apellido del cliente</label>
-        <input type="text" id="lname" name="lastname">
+        <input type="text" id="lname" name="lastname" v-model="apellido">
 
         <label>Localidad</label>
-        <select id="country" name="country">
+        <select id="city" v-model="ciudad">
           <option value="buenos aires">CABA</option>
           <option value="cba">Córdoba</option>
           <option value="ros">Rosario</option>
@@ -19,14 +23,11 @@
 
         <label> Fecha </label>
         <br> <br>
-        <input type="date">
+        <input type="date" v-model="fecha">
         <br><br>
         <label>Breve descripción del proyecto </label>
-        <textarea id="subject" name="subject" style="height:200px"></textarea>
+        <textarea id="description" v-model="descripcion" ></textarea>
 
-        <input type="text" v-model="title">
-        <input type="text" v-model="client">
-        <input type="text" v-model="date">
         <button @click= "crearProyecto()" > CREAR PROYECTO </button>
         {{listaObjetos}}
       </form>
@@ -51,20 +52,29 @@ export default {
   data() {
     return {
       listaObjetos: [],
-      title: "",
-      client: "",
-      date: ""
+      nombre_proyecto: "",
+      nombre: "",
+      apellido: "",
+      ciudad: "",
+      fecha: "",
+      descripcion: ""
     }
   },
   methods: {
     crearProyecto() {
-      let titulo = this.title
-      let cliente = this.client
-      let fecha = this.date
+      let p_name = this.nombre_proyecto
+      let name = this.nombre
+      let lastname = this.apellido
+      let city = this.ciudad
+      let date = this.fecha
+      let description = this.descripcion
       let projectObject = {
-        "titulo" : titulo,
-        "cliente": cliente,
-        "fecha": fecha
+        "nombre del proyecto": p_name,
+        "nombre del cliente": name,
+        "apellido": lastname,
+        "ciudad": city,
+        "fecha": date,
+        "descripcion": description
       }
       this.listaObjetos.push(projectObject)
     }
@@ -104,5 +114,9 @@ input[type=submit]:hover {
   border-radius: 5px;
   background-color: #f2f2f2;
   padding: 20px;
+}
+
+textarea {
+  height:200px
 }
 </style>
