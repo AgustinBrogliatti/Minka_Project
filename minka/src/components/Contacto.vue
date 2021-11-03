ec3b17a8bac7a76cdfd66b5b7e7ff319d3e6b68e
MartinoBoca <bocamartino1201@gmail.com>
2021-11-03 11:26:21 -0300

    views: contact, fileerror

--- minka/src/components/Contacto.vue
+++ minka/src/components/Contacto.vue
@@ -1,15 +1,43 @@
 <template>
-  <div>
+  <div id="home_page">
+    <Header></Header>
+    <div id="content-body">
+      <NavBar></NavBar>
+      <div class="content-home">
+        <h2> Nombre arquitecto: </h2>
+        <p> {{nombre}} </p>
+      </div>
 
+      <Banner></Banner>
+    </div>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
+
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Banner from "@/components/Banner";
+
+
 export default {
-  name: "Contacto"
+  name: "Contacto",
+  components:
+  Header,
+  Footer,
+  NavBar,
+  Banner,
+  data() {
+    return {
+      nombre: "Raul Pedro García"
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+@import "../assets/CSS/main layout.css";
+@import "../assets/CSS/normalize.css";
 </style>
