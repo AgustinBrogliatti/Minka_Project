4c336f034fd69faee83d08023efca5e846d91be7
mateosceia <mateosceia.ms@gmail.com>
2021-11-04 17:50:14 -0300

    perfil vista(base)

--- minka/src/components/MiPerfil.vue
+++ minka/src/components/MiPerfil.vue
@@ -1,24 +1,71 @@
 <template>
-  <div>
-    <HeaderPortada/>
+  <div id="profile">
+    <Header/>
+    <div id="content-body">
+      <NavBar></NavBar>
+      <div class="content-home">
+        <h1>{{nombre}}</h1>
+        <h2>Datos:</h2>
+        <p>Usuario: {{usuario}}</p>
+        <p>Mail: {{mail}}</p>
+        <button class="logout" @click="goToStart()">Cerrar Sesión</button>
+      </div>
+      <Banner></Banner>
+
+    </div>
     <Footer/>
-    <router-link to="/home">Volver atrás</router-link>
+
   </div>
 </template>
 
 <script>
-import HeaderPortada from "./HeaderPortada";
+import Header from "./Header";
 import Footer from "./Footer";
+import NavBar from "./NavBar";
+import Banner from "./Banner";
 
 export default {
   name: "MiPerfil",
-  components:{
-    HeaderPortada,
-    Footer
-  }
+  components: {
+    Header,
+    Footer,
+    NavBar,
+    Banner
+  },
+  methods: {
+    goToStart() {
+      this.$router.push('/');
+    },
+  },
+    data() {
+      return {
+        nombre: "Agustin Brogliati",
+        usuario: "agusbrogliatti",
+        mail: "agustinbrogliatti@gmail.com"
+
+      }
+    }
 }
 </script>
 
 <style scoped>
+.content-home{
+  display: flex;
+  flex-direction: column;
+}
+.logout{
+  background-color: var(--main-color);
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  border-radius: 8px;
+}
+.logout:hover {
+  box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
 
 </style>
